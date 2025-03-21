eea50dff3804b93a87a2648a6d0e61a9db59ec01
NazarovBehruzbek <behruzbeknazarov001@gmail.com>
2025-03-21 10:42:27 +0500

    add

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ mongoose
 app.use("/api/course", courseRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server ishga tushdi port: ${PORT}`));
